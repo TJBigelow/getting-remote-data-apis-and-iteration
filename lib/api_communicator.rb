2a1ede4e116fe9753e58c4af929cef38735fdadd
tjbigelow <tjbigelow@att.net>
2020-09-28 12:58:38 -0500

    Added search functionality

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,11 +4,11 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
-  response_string = RestClient.get('http://swapi.dev/api/people')
+  response_string = RestClient.get("http://swapi.dev/api/people/?search=#{character_name}")
   response_hash = JSON.parse(response_string)
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
-  character_films = response_hash['results'].find{|hash| hash['name'].downcase == character_name}['films']
+  character_films = response_hash['results'][0]['films']
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   film_apis = collect_apis(character_films)
@@ -18,6 +18,12 @@ def get_character_movies_from_api(character_name)
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
   film_apis.map{|api| JSON.parse(api)}
+end
+
+def searched_character_name(character_name)
+  response_string = RestClient.get("http://swapi.dev/api/people/?search=#{character_name}")
+  response_hash = JSON.parse(response_string)
+  response_hash['results'][0]['name']
 end
 
 def collect_apis(apis)
@@ -31,6 +37,7 @@ end
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
+  puts "#{searched_character_name(character)} is in:"
   puts print_movies(films)
 end
 
